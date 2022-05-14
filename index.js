461fd63ab1b9505a71031161a06b233e9f35ab54
jung su young <j9972@naver.com>
2022-05-14 16:04:52 +0900

    get / set

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ const client = redis.createClient();
 client.connect();
 
 // util.promisify => 비동기로 돌리려는 함수를 promise로 감싸주지 않고 사용할 수 있다.
-//client.set = util.promisify(client.set);
-client.set("post", "test");
+// client.set = util.promisify(client.set);
+// client.get = util.promisify(client.get);
+
 const app = express();
 app.use(express.json());
 
 app.post("/", async (req, res) => {
   const { key, value } = req.body;
-  let respone_data = client.set(key, value);
-  console.log(respone_data);
-  const response = await client.get(key);
-  res.json(respone_data);
+  client.set(key, value);
+
+  const response = await client.get(key, value);
+  console.log("success");
+  res.json(response);
 });
 
 app.listen(8080, () => {
